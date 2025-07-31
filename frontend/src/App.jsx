041d0fc49828e831959be6c9cf85ba166ceefaa5
Vaishnavi Jha <140975686+vaishnavigithub899@users.noreply.github.com>
2025-07-31 22:55:07 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://ai-code-reviewer-backend-seven.vercel.app', { code })
     setReview(response.data)
   }
 
